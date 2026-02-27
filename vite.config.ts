b0b8e1d177d65b116dcd19c0f6213ec156c1f342
a13o <2031379+a13o@users.noreply.github.com>
2026-02-27 13:14:02 +0100

    first pass, switch to generated pwa assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import {defineConfig, transformWithEsbuild} from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import {VitePWA} from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
@@ -19,6 +20,42 @@ export default defineConfig({
     },
     react({
       include: /\.(jsx|js|tsx|ts)$/,
+    }),
+    VitePWA({
+      injectRegister: false,
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Cross with Friends',
+        short_name: 'CWF',
+        description: 'Solve crossword puzzles together with friends in real time.',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '.',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
     }),
   ],
   resolve: {
